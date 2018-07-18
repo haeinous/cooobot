1e4e062a8ec9cfaf08274322b919914803df4327
Hae-in Lim <haeinous@gmail.com>
2018-07-18 15:44:34 -0700

    Updated secrets.sh with new API credentials.

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-import tweepy
-import random
-import time
-from credentials import *
+import tweepy, time, os
+from random import randint
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(os.environ.get(consumer_key), os.environ.get(consumer_secret))
+auth.set_access_token(os.environ.get(access_token), os.environ.get(access_token_secret))
 api = tweepy.API(auth)
 
 
 class StreamListener(tweepy.StreamListener):
-  def on_status(self, tweet):
+    def on_status(self, tweet):
 
-    #retweets of tweets containing @cooobot are ignored
-    if tweet.retweeted_status:
-      return
+        #retweets of tweets containing @cooobot are ignored
+        if tweet.retweeted_status:
+            return
 
-    #print(tweet)
-    username = tweet.user.screen_name
-    text = '@' + username + ' cooo'
-    tweet_id = tweet.id
-    
-    #random time delay makes it suspenseful
-    time.sleep(random.randint(1,45))
-    api.update_status(status = text, in_reply_to_status_id = tweet_id)
+        #print(tweet)
+        username = tweet.user.screen_name
+        text = '@' + username + ' cooo'
+        tweet_id = tweet.id
+        
+        #random time delay makes it suspenseful
+        time.sleep(randint(1, 45))
+        api.update_status(status = text, in_reply_to_status_id = tweet_id)
 
-  def on_error(self, status_code):
-    if status_code == 420:
-       print('Rate limited!')
+    def on_error(self, status_code):
+        if status_code == 420:
+            print('Rate limited!')
 
-       return False
+            return False
 
 def stream(searchItems):
-  streamListener = StreamListener()
-  stream = tweepy.Stream(auth = api.auth, listener = streamListener)
-  stream.filter(track=searchItems)
+    streamListener = StreamListener()
+    stream = tweepy.Stream(auth = api.auth, listener = streamListener)
+    stream.filter(track = searchItems)
 
 
 try:
-  stream(['@cooobot'])
+    stream(['@cooobot'])
 except:
-  print("Failure")
+    print('failure')
+finally:
+    print('finally')
+
+
