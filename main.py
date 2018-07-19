f0f4d28e5bd5a59268fd779ce0e020e6041ac2e2
Hae-in Lim <haeinous@gmail.com>
2018-07-18 17:24:06 -0700

    Debugged @cooobot and added docstrings.

--- main.py
+++ main.py
@@ -1,44 +1,54 @@
 import tweepy, time, os
 from random import randint
 
-auth = tweepy.OAuthHandler(os.environ.get(consumer_key), os.environ.get(consumer_secret))
-auth.set_access_token(os.environ.get(access_token), os.environ.get(access_token_secret))
-api = tweepy.API(auth)
-
 
 class StreamListener(tweepy.StreamListener):
+    
+    """
+    Class inherting from tweepy's StreamListener for @cooobot tweets
+    """
+    
     def on_status(self, tweet):
-
-        #retweets of tweets containing @cooobot are ignored
-        if tweet.retweeted_status:
-            return
-
-        #print(tweet)
+        """
+        Called when a new status arrives.
+        """
         username = tweet.user.screen_name
         text = '@' + username + ' cooo'
         tweet_id = tweet.id
         
-        #random time delay makes it suspenseful
-        time.sleep(randint(1, 45))
-        api.update_status(status = text, in_reply_to_status_id = tweet_id)
+        time.sleep(randint(1, 45))  # random time delay makes it suspenseful
+        api.update_status(status=text, in_reply_to_status_id=tweet_id)
+
 
     def on_error(self, status_code):
+        """
+        Called when a non-200 status code is returned
+        """
         if status_code == 420:
             print('Rate limited!')
-
+            return False
+        else:
+            print('other error: {}'.format(status_code))
             return False
 
-def stream(searchItems):
-    streamListener = StreamListener()
-    stream = tweepy.Stream(auth = api.auth, listener = streamListener)
-    stream.filter(track = searchItems)
+
+def stream(search_items):
+    """
+    Creates the stream listener.
+    """
+    stream_listener = StreamListener()
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+    stream.filter(track=search_items, async=True)
 
 
-try:
-    stream(['@cooobot'])
-except:
-    print('failure')
-finally:
-    print('finally')
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'))
+    auth.set_access_token(os.environ.get('access_token'), os.environ.get('access_token_secret'))
+    api = tweepy.API(auth)
 
-
+    try:
+        stream(['@cooobot'])
+    except:
+        print('failure')
+    finally:
+        print('finally')
